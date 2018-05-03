b30169462e4983d736a8f753b657c69e45657998
kenny lee <kennylee@kennydeMacBook-Pro.local>
2018-05-04 00:59:29 +0800

    Init commit

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,29 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-admobpro.AdMob",
+    "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+    "pluginId": "cordova-plugin-admobpro",
+    "clobbers": [
+      "window.AdMob"
+    ]
+  },
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-extension": "1.5.4",
+  "cordova-plugin-admobpro": "2.31.5",
+  "cordova-plugin-inappbrowser": "3.0.0"
+};
+// BOTTOM OF METADATA
+});
